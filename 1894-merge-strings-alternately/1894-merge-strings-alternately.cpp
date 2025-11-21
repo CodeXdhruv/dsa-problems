8af09c20be5fccf814dbcc81c920038365d4a9c4
Dhruv Sen <dhruvsen24@gmail.com>
2025-11-21 10:16:06 +0530

    Time: 3 ms (39.72%), Space: 8.5 MB (32.56%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string word3="";
+        int i=0;
+        int j=0;
+        while(i<word1.size() && j<word2.size()){
+            word3.push_back(word1[i++]);
+            word3.push_back(word2[j++]);
+        }
+        while(i<word1.size()){word3.push_back(word1[i++]);}
+        while(j<word2.size()){word3.push_back(word2[j++]);}
+        return word3;
+    }
+};
